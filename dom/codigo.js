f91408ecd18130fff072a094b17938db6e4e7a4a
Leonardo Martin Jimenez <leomartinjimenez@gmail.com>
2018-03-13 15:55:02 +0100

    new projects

---
+++ dom/codigo.js
@@ -0,0 +1,7 @@
+var ruta = window.location;
+
+console.log('Estoy depurando LOG, mensaje secreto');
+console.log(ruta);
+console.log(window);
+console.log(navigator);
+document.write('Estás en: ' + ruta.origin);
