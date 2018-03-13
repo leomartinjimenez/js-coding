1ee7381400281726d186733787df4856ffdc3b61
Leonardo Martin Jimenez <leomartinjimenez@gmail.com>
2018-03-13 15:50:49 +0100

    Revert "-. all the initial current files which compounds the project."
    This reverts commit b57250383adcfd197163bd21da287d43a24043d2.

--- README.md
+++
@@ -1,2 +0,0 @@
-# js-coding-drawWithArrowKeys
-JavaScript code for draw over a canvas with the arrow keys.
--- js-coding-dom/codigo.js
+++
@@ -1,7 +0,0 @@
-var ruta = window.location;
-
-console.log('Estoy depurando LOG, mensaje secreto');
-console.log(ruta);
-console.log(window);
-console.log(navigator);
-document.write('Estás en: ' + ruta.origin);
