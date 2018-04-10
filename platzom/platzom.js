95cc7139a5f96801c4e9d663ef06c78bfdc57f5b
Leonardo Martin Jimenez <leomartinjimenez@gmail.com>
2018-04-10 20:59:07 +0200

    -. New logics.

--- platzom/platzom.js
+++ platzom/platzom.js
@@ -1,6 +1,6 @@
 /**
  * @Date:   2018-04-04T01:37:42+02:00
- * @Last modified time: 2018-04-04T01:52:01+02:00
+ * @Last modified 2018-04-09
  */
 function platzom(str){
   let translation = str;
@@ -11,15 +11,68 @@ function platzom(str){
     translation = str.slice(0, -2); // from 0 to 2 characters counts from the ends
   }
 
-  // case 1: if the word starts with "Z", you have to add "pe" in the end of the word
+  // case 2: if the word starts with "Z", you have to add "pe" in the end of the word
   // example: zorro --> zorrope
   if (str.toLowerCase().startsWith('z')) {
     //translation = translation + 'pe';
     translation += 'pe';
   }
+
+  // case 3: if the "translation" word has a leght = or > 10 characters, divide by 2 and insert a middle -
+  // example: zorro --> zorrope
+  translationLenght = translation.length;
+  if (translation.length>=10) {
+    const firstHalf = translation.slice(0,Math.round(translationLenght/2));
+    const secondHalf = translation.slice(Math.round(translationLenght/2),translationLenght);
+    translation = firstHalf + '-' + secondHalf;
+  }
+
+  // case 4: if the word is a "Palindromic". Example: "rotator"
+  // at this case, to create a word with the pattern lower+upper+lower+.... case
+  // Example: rOtAtOr
+
+  function reverse(str) { 
+    return str.split('').reverse().join('');
+  }
+
+  function lowerUpperWord(str) { 
+    const stringLength = str.length;
+    let translation = '';
+    let capitalize = true;
+
+    let i = 0;
+    while (i < stringLength) {
+      // statement
+      const currentChar = str.charAt(i);
+
+      
+      if (capitalize == true) {
+        translation += currentChar.toUpperCase();
+      } else {
+        translation += currentChar.toLowerCase();
+      }
+      
+      // translation += capitalize ? currentChar.toUpperCase() : currentChar.toLowerCase();
+
+      capitalize = !capitalize;
+
+      i++;
+    }
+   return translation;
+  }
+
+  if (str == reverse(str)) {
+    //console.log('IS reverse!!! ');
+    return lowerUpperWord(str);
+  }
+
+
   return translation;
 }
 
 console.log(platzom('Programar')); // Result --> Program
 console.log(platzom('Zorro')); // Result --> Zorrope
 console.log(platzom('Zarpar')); // Result --> Zarppe
+console.log(platzom('Supercalifragilisticoespialidoso')); // Result --> Zarppe
+console.log(platzom('rotator')); // Result --> rOtAtOr
+alert(platzom('rotator'));
---
+++ superHeroesBattle/superHeroesBattle.js
@@ -0,0 +1,66 @@
+alert('The battle is about to start: are you ready? ');
+
+let lifeGoku = 100;
+let lifeSuperman = 100;
+
+const MIN_POWER = 5;
+const MAX_POWER = 12;
+
+let round = 0;
+
+function bothAreAlive () {
+	let answer = false;
+	if (lifeGoku > 0 && lifeSuperman > 0) {
+		answer = true;
+	}
+
+	return answer
+}
+
+function calculateHit () {
+	return ( Math.round((Math.random() * (MAX_POWER-MIN_POWER) + MIN_POWER)) );
+}
+
+console.log('*** START the battle: ***');
+console.log('-. GOKU has the INIT life: ' + lifeGoku);
+console.log('-. SUPERMAN has the INIT life: ' + lifeSuperman);
+
+while ( bothAreAlive() ) {
+	
+	round++;
+	console.log('Round: ' + round);
+
+	const hitGoku = calculateHit();
+	const hitSuperman = calculateHit();
+
+	if (hitGoku > hitSuperman) {
+		// Goku attacks
+		console.log('Goku attacks Superman --> Hit = ' + hitGoku);
+		lifeSuperman -= hitGoku;
+		// lifeSuperman = lifeSuperman - hitGoku;
+		console.log('-. SUPERMAN has the current life: ' + lifeSuperman);
+	} else {
+		// Superman attacks
+		console.log('Superman attacks Goku --> Hit = ' + hitSuperman);
+		lifeGoku -= hitSuperman;
+		// lifeGoku = lifeGoku - hitSuperman;
+		console.log('-. GOKU has the current life: ' + lifeGoku);
+	}
+	
+}
+
+function gokuKeepAlive () {
+	let answer = false;
+	if (lifeGoku > 0) {
+		answer = true;
+	}
+	return answer;
+}
+
+if (gokuKeepAlive()) {
+	console.log('The WINNER is: ' + 'GOKU !!! :)');
+	alert('The WINNER is: ' + 'GOKU !!! :)');
+} else {
+	console.log('The WINNER is: ' + 'SUPERMAN !!! :)');
+	alert('The WINNER is: ' + 'SUPERMAN !!! :)');
+}
